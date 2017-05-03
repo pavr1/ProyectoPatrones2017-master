01c58301ab1d673837fa79d0a94047278df84207
pavr1 <pavr1@hotmail.com>
2017-05-02 20:05:44 -0600

    Deleted line newJavamon.setElement(Element.valueOf(randomGenerator.nextInt(4-0+1)+0 )); since the javamonBuilder already gets the element

--- src/Main/MindGames.java
+++ src/Main/MindGames.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package mindgames;
+package Main;
 
 import games.Checkers;
 import games.Chess;
