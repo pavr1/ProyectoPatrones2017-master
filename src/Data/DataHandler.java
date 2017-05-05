49597082473df6644f9188530255646908dce178
Jose Madrigal <jmadrigal_94@outlook.com>
2017-05-04 21:50:20 -0600

    Se agregan funcionalidades a Gestor y DataHandler

--- src/Data/DataHandler.java
+++ src/Data/DataHandler.java
@@ -1,14 +1,98 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Data;
 
-/**
- *
- * @author pavr1
- */
+import games.Interfaces.IGame;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.Properties;
+
 public class DataHandler {
-    
+
+    private InputStream inputStream;
+    private Reader inputStreamReader;
+    private ArrayList<String> userList;
+
+    public DataHandler() {
+        userList = new ArrayList<String>();
+    }
+
+    public StringBuilder readFile(String pfile) throws Exception {
+        BufferedReader br;
+        String currentLine;
+        StringBuilder fileData = new StringBuilder();
+
+        // if (verifyFile(pfile)) {
+        br = new BufferedReader(new FileReader("src/properties/" + pfile));
+        while ((currentLine = br.readLine()) != null) {
+            //System.out.println(currentLine);
+            fileData.append(currentLine).append("\n");
+        }
+        //}
+
+        return fileData;
+    }
+
+    public void writeFile(String pfile, String pextension, String pfileData) throws Exception {
+        File file;
+
+        //if (!verifyFile(pfile)) {
+        file = new File("src/properties/" + pfile + "." + pextension);
+        FileWriter writer = new FileWriter(file);
+        writer.write(pfileData);
+        writer.close();
+        // }
+    }
+
+    public void registerUserFile(String pusername, String ppassword) throws Exception {
+        Properties prop = new Properties();
+        OutputStream output;
+
+        if (!verifyUser(pusername)) {
+            String file = "src/properties/" + "Usr" + pusername + ".properties";
+            output = new FileOutputStream(file);
+            prop.setProperty("Username", pusername);
+            prop.setProperty("Password", ppassword);
+            userList.add(file);
+            prop.store(output, null);
+        }
+    }
+
+    public boolean verifyUser(String pusername) throws Exception {
+        Properties prop = new Properties();
+        InputStream input;
+        boolean userExist = false;
+        String username;
+
+        for (int i = 0; i < userList.size(); i++) {
+
+            input = new FileInputStream(userList.get(i));
+            prop.load(input);
+            username = prop.getProperty("Username");
+
+            if (username.equals(pusername)) {
+                userExist = true;
+            }
+        }
+
+        return userExist;
+    }
+
+    /* public boolean verifyFile(String pfile) throws Exception {
+        boolean fileExist = false;
+
+        FileReader fr = new FileReader("src/properties/" + pfile);
+        
+            fileExist = true;
+        
+
+        return fileExist;
+    }*/
 }
--- src/Main/Gestor.java
+++ src/Main/Gestor.java
@@ -1,14 +1,71 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Main;
 
-/**
- *
- * @author pavr1
- */
+import Authentication.User;
+import Data.DataHandler;
+import Factory.GameFactory;
+import games.Enumerations.GameTypes;
+import games.Interfaces.IGame;
+import games.Interfaces.IPiece;
+
 public class Gestor {
-    
+
+    private GameFactory gameFactory;
+    private User user;
+    private IGame currentGame;
+    private DataHandler dataHandler;
+
+    public Gestor() {
+        dataHandler = new DataHandler();
+    }
+
+    public boolean checkMovement(IPiece piece) {
+        //return currentGame.checkMove(piece);
+        return false;
+    }
+
+    public void createGame(GameTypes pgameType) {
+        //currentGame = GameFactory.gameSerializer(pgameType);
+    }
+
+    public StringBuilder loadGame(String pfileName) {
+        StringBuilder fileData = new StringBuilder();
+
+        try {
+            fileData = dataHandler.readFile(pfileName);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        return fileData;
+    }
+
+    public void saveGame() {
+
+    }
+
+    public StringBuilder printGame(String pfileName) {
+
+        StringBuilder gameData = new StringBuilder();
+        try {
+            gameData = dataHandler.readFile(pfileName);
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        return gameData;
+    }
+
+    public boolean createUser(String pusername, String pemail, String ppassword) throws Exception {
+
+        boolean userCreated = false;
+
+        if (!dataHandler.verifyUser(pusername)) {
+            //user = new User(pname, pemail, ppassword);
+            dataHandler.registerUserFile(pusername, ppassword);
+            userCreated = true;
+        }
+
+        return userCreated;
+    }
 }
